27dc01d0572059d9a74f27708880540887b410c6
Pedro Cruz <142250942+pmzpro@users.noreply.github.com>
2024-06-10 17:18:16 +0100

    Added 5 select joins

--- BaseDeDados/selects.sql
+++ BaseDeDados/selects.sql
@@ -90,4 +90,98 @@ GROUP BY
 HAVING 
     AVG(reviews.ratings) >= 4;
 
+-- Obtenha os detalhes dos produtos juntamente com os nomes das plataformas em que estão disponíveis.
+SELECT 
+    p.name AS product_name,
+    p.price,
+    pl.name AS platform_name
+FROM 
+    products p
+JOIN 
+    products_platforms pp ON p.id = pp.fk_products_id
+JOIN 
+    platforms pl ON pp.fk_platforms_id = pl.id;
+    
+-- Obtenha os detalhes dos desenvolvedores juntamente com a média de classificações das suas avaliações.
+SELECT 
+    d.name AS developer_name,
+    ROUND(AVG(r.ratings), 0) AS average_rating
+FROM 
+    developers d
+LEFT JOIN 
+    products p ON d.id = p.fk_developers_id
+LEFT JOIN 
+    reviews r ON p.id = r.fk_product_id
+WHERE 
+    r.ratings IS NOT NULL
+GROUP BY 
+    d.id;
+    
+-- Obtenha os detalhes das vendas juntamente com o nome do distribuidor e o número de produtos vendidos em cada venda.
+SELECT 
+    s.id AS sale_id,
+    s.date,
+    d.name AS distributor_name,
+    COUNT(sp.fk_products_platforms_id) AS product_count
+FROM 
+    sales s
+JOIN 
+    distributors d ON s.fk_distributors_id = d.id
+JOIN 
+    sales_products sp ON s.id = sp.fk_sales_id
+GROUP BY 
+    s.id;
+    
+-- Obtenha a quantidade de produtos, clientes, desenvolvedores e fornecedores.
+
+SELECT 
+    'Products' AS Entity,
+    COUNT(*) AS Count
+FROM 
+    products
+UNION ALL
+SELECT 
+    'Customers' AS entity,
+    COUNT(*) AS count
+FROM 
+    customers
+UNION ALL
+SELECT 
+    'Developers' AS entity,
+    COUNT(*) AS count
+FROM 
+    developers
+UNION ALL
+SELECT 
+    'Suppliers' AS entity,
+    COUNT(*) AS count
+FROM 
+    suppliers;
+
+-- Obtenha detalhes do cliente, jogo comprado, total gasto e detalhes do pagamento.
+SELECT 
+    c.id AS customer_id,
+    c.address AS customer_address,
+    c.phone_number AS customer_phone,
+    p.name AS product_name,
+    ROUND(SUM(sp.price), 2) AS total_spent,
+    pay.card_name AS payment_card_name,
+    pay.card_number AS payment_card_number,
+    pay.due_date AS payment_due_date
+FROM 
+    customers c
+JOIN 
+    customers_payments cp ON c.id = cp.fk_customers_id
+JOIN 
+    payments pay ON cp.fk_payments_id = pay.id
+JOIN 
+    sales s ON cp.fk_sales_id = s.id
+JOIN 
+    sales_products sp ON s.id = sp.fk_sales_id
+JOIN 
+    products_platforms pp ON sp.fk_products_platforms_id = pp.id
+JOIN 
+    products p ON pp.fk_products_id = p.id
+GROUP BY 
+    c.id, c.address, c.phone_number, p.name, pay.card_name, pay.card_number, pay.due_date;
     
